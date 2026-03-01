5a88dfb5aa13546e79681adf2fd4d5f2d3097c7d
Stealth Shark <jaugustin316@aol.com>
2026-03-01 02:17:54 -0500

    variables

---
+++ modules/scps/variables.tf
@@ -0,0 +1,16 @@
+variable "policies" {
+  description = "Map of SCP policies to create. Key is used as the resource identifier."
+  type = map(object({
+    name = string
+    content = string
+  }))
+}
+
+variable "attachments" {
+  description = "List of SCP policy attachments"
+  type = list(object({
+    policy_key = string
+    target_id = string
+  }))
+}
+
