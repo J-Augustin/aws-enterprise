27605cb4d9fac181aa26a81e2d270aa501053953
Stealth Shark <jaugustin316@aol.com>
2026-03-03 21:43:57 -0500

    delete unused variables

--- variables.tf
+++ variables.tf
@@ -8,13 +8,3 @@ variable "security_account_id" {
   description = "AWS Account ID for the Security OU where security tools will be deployed"
   type        = string
 }
-
-variable "log_archive_account_id" {
-  description = "AWS Account ID for Log Archive account"
-  type        = string
-}
-
-variable "organization_name" {
-  description = "Name of the AWS Organization"
-  type        = string
-}
